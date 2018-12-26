9a431beb7ce6877213e1c044430cb072123dbe8e
[Huang] <24320162202837@stu.xmu.edu.cn>
2018-12-26 22:52:09 +0800

    登录

--- src/components/LoginPart/login.vue
+++ src/components/LoginPart/login.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app-login">
+    <el-main>
     <el-row type="flex">
       <el-col :span="24" Typography:微软雅黑 >
         <p style="color: #494e8f;font-size: 1.6rem;">欢迎登录课程管理系统</p>
@@ -10,34 +11,23 @@
         <img src="../../assets/XMU.png" style="width: 8rem">
       </el-col>
     </el-row>
-    <el-form ref="form" :rules="rules"><!---->
+    <el-form :model="form" ref="form" :rules="rules"><!---->
       <el-form-item prop="user">
-        <el-row type="flex" justify="center" class="row-height">
-          <el-col :span="20">
-            <el-input class="activeInput" placeholder="请输入学号/教工号/管理员账号" v-model="form.user" size="large" clearable></el-input>
-          </el-col>
-        </el-row>
+        <el-input class="activeInput" placeholder="请输入学号/教工号/管理员账号" v-model="form.user" clearable></el-input>
       </el-form-item>
       <el-form-item prop="password">
-        <el-row type="flex" justify="center" class="row-height">
-          <el-col :span="20">
-            <el-input class="activeInput" type="password" placeholder="请输入密码" v-model="form.password" size="large" clearable></el-input>
-          </el-col>
-        </el-row>
+        <el-input class="activeInput" type="password" placeholder="请输入密码" v-model="form.password" clearable></el-input>
       </el-form-item>
       <el-form-item>
         <el-radio v-model="form.identity" label="1">教师</el-radio>
         <el-radio v-model="form.identity" label="0">学生</el-radio>
       </el-form-item>
       <el-form-item>
-        <el-row  type="flex" justify="center" style="margin-buttom: 5rem;">
-          <el-col>
-            <el-button type="primary" class="login-button" @click="sendUserInfo">登录</el-button>
-          </el-col>
-        </el-row>
+        <el-button type="primary" class="login-button" @click="sendUserInfo('form')">登录</el-button>
       </el-form-item>
     </el-form>
     <span class="forgetPsw" @click="forgetPsw">忘记密码</span>
+    </el-main>
   </div>
 </template>
 
@@ -45,6 +35,22 @@
     export default {
       name:"Login",
         data(){
+        var validatePsw=(rule,value,callback)=>{
+          if(value===''){
+            callback(new Error('请输入密码'));
+          }else if(value.length<6){
+            callback(new Error('密码不得少于6位'));
+          }else{
+            callback();
+          }
+        };
+        var validateUser=(rule,value,callback)=>{
+          if(value===''){
+            callback(new Error('账号不能为空'));
+          }else{
+            callback();
+          }
+        };
           return{
             form:{
               user:'',
@@ -53,50 +59,60 @@
             },
             rules:{
               user:[
-                { required:true ,message:'用户名不能为空！',trigger: 'blur' }
+                {validator:validateUser,trigger:'blur'}
               ],
               password:[
-                { required:true ,message:'密码不能为空！',trigger: 'blur'},
-                { min:6 ,message:'长度不少于六位',trigger: 'blur' }
+                {validator:validatePsw,trigger:'blur'}
               ]
             },
           }
         },
       methods: {
-        sendUserInfo(){
+        sendUserInfo(formName){
           var that=this;
-          this.$axios({
-            method:'post',      //传输方法
-            url:"http://kt35um.natappfree.cc/user/login",   //路径
-            data:{                                //传给后端的数据（后端数据名：……）
-              account:this.form.user,
-              password:this.form.password,
-              type:parseInt(this.form.identity),
+          this.$refs[formName].validate((valid)=>{
+            if(valid){
+              this.$axios({
+                method:'post',      //传输方法
+                url:"http://hqfjfz.natappfree.cc/user/login",   //路径
+                data:{                                //传给后端的数据（后端数据名：……）
+                  account:this.form.user,
+                  password:this.form.password,
+                  type:parseInt(this.form.identity),
+                }
+              })
+                .then(                //成功后的操作
+                  function (response) {
+                    if(response.data.message===0)
+                    {
+                      if(that.form.identity==='1'){
+                        that.$router.push({path:"/ActiveTeacher",query:{account:that.form.user}});
+                      }
+                      else if(that.form.identity==='0'){
+                        that.$router.push("/ActiveStudent");
+                      }
+                      else{
+                        console.log('Error');
+                      }
+                    }
+                    else if(response.data.message===1)
+                    {
+                      that.$router.push("/");
+                      //跳转页面没写
+                    }
+                    else{
+                      alert("登录失败！");
+                    }
+                  })
+                .catch(function(error){
+                  console.log(error);
+                });
+            }else{
+              console.log('error submit!!');
+              return false;
             }
-          })
-            .then(                //成功后的操作
-              function (response) {
-                if(response.data.message===0)
-                {
-                  if(that.form.identity==='1'){
-                    that.$router.push({path:"/ActiveTeacher",query:{account:this.form.user}});
-                  }
-                  else if(that.form.identity==='0'){
-                    that.$router.push("/ActiveStudent");
-                  }
-                  else{
-                    console.log('Error');
-                  }
-                }
-                else if(response.data.message===1)
-                {
-                  that.$router.push("/");
-                }
-                else{
-                  alert("登录失败！");
-                }
-              }
-            )
+          });
+
         },
         forgetPsw(){
           this.$router.push('/ForgetPsw')
