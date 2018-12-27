bc8d98fbf613d37cd2d0e380675dcc8931aa2171
tao2010 <45806680+tao2010@users.noreply.github.com>
2018-12-27 20:18:09 +0800

    Add files via upload

---
+++ src/components/studentSeminar/studentViewPpt.vue
@@ -0,0 +1,55 @@
+<template>
+  <el-container style="height:100%;width: 100%; margin:0 auto;">
+    <el-header id="header">
+      <el-button class="el-icon-back" ></el-button>
+      <p>{{headerLocation}}</p>
+      <el-dropdown>
+        <el-button class="el-icon-menu"></el-button>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item>代办</el-dropdown-item>
+          <el-dropdown-item>个人页面</el-dropdown-item>
+          <el-dropdown-item>讨论课</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+    </el-header>
+    <el-main>
+
+      <el-table :data="tableData">
+        <el-table-column prop="team" label="组别" width="100%">
+        </el-table-column>
+        <el-table-column prop="ppt" label="演示文档" width="200%">
+        </el-table-column>
+      </el-table>
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        headerLocation: "OOAD-讨论课",
+        tableData: [{
+          team: '第一组',
+          ppt: '1-1业务流程.ppt '
+        }, {
+          team: '第二组',
+          ppt: '1-2 未提交'
+        }, {
+          team: '第三组',
+          ppt: '可报名 '
+        }, {
+          team: '第四组',
+          ppt: '1-4业务流程.ppt '
+        }]
+      }
+    }
+  }
+</script>
+<style>
+  .el-container {
+    margin-bottom: 40px;
+    color: #333;
+    background-color:white;
+  }
+</style>
