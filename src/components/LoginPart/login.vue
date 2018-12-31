f32337f2ccf6642ac472dbae92734ac34159e80e
[Huang] <24320162202837@stu.xmu.edu.cn>
2018-12-31 22:38:11 +0800

    jwt 登录界面&配置

--- src/components/LoginPart/login.vue
+++ src/components/LoginPart/login.vue
@@ -87,7 +87,6 @@
               })
                 .then(response=>{
                   _this.$store.commit("SET_AUTH",response);
-                  console.log(_this.$store.state.token);
                   if(_this.role==="teacher"){
                     if(response.active===0) _this.$router.push('/ActiveTeacher');
                     else if(response.active===1) _this.$router.push('/TeaPersonCenter');
